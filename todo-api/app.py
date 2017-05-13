2358c8e348b68e730dd167aca49f28c933d493b2
Jacob Liberman <jliberma@redhat.com>
2017-05-12 23:39:56 -0500

    add TODO and latest functions

--- todo-api/app.py
+++ todo-api/app.py
@@ -4,6 +4,7 @@ from flask import jsonify
 from flask import abort
 from flask import make_response
 from flask import request
+from flask import url_for
 
 
 app = Flask(__name__)
@@ -27,7 +28,7 @@ tasks = [
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-        return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
@@ -55,6 +56,46 @@ def create_task():
     }
     tasks.append(task)
     return jsonify({'task': task}), 201
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and \
+            type(request.json['description']) is not unicode:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description',
+                                              task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'],
+                                      _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
 
 if __name__ == '__main__':
     app.run(debug=True)
